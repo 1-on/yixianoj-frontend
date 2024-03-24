8061c4a74fa2b6efdae8499c6b144d33074db3c3
一弦 <2721975880@qq.com>
2024-03-24 14:12:09 +0800

    基本完成

--- generated/core/OpenAPI.ts
+++ generated/core/OpenAPI.ts
@@ -20,7 +20,7 @@ export type OpenAPIConfig = {
 };
 
 export const OpenAPI: OpenAPIConfig = {
-  BASE: "http://localhost:8101",
+  BASE: "http://114.55.246.220:8101",
   VERSION: "1.0",
   WITH_CREDENTIALS: true,
   CREDENTIALS: "include",
